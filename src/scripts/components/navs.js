edc3d42f9481809923dbd3caf7c1aa0a6bb1d9cc
rkingston <r.kingston@northeastern.edu>
2017-09-15 10:32:48 -0400

    update nav class naming convention

--- src/scripts/components/navs.js
+++ src/scripts/components/navs.js
@@ -5,11 +5,11 @@
 //
 // Tabbed Navigation, nested
 
-$('.nav__link').on('shown.bs.tab', e => {
+$('.__link').on('shown.bs.tab', e => {
   $(e.relatedTarget).removeClass('active')
-  $('.nav__item--haschildren a').removeClass('child--active')
+  $('.\\+children a').removeClass('child--active')
 });
 
-$('.nav__child-link').on('click touch', function(e) {
+$('.__child-link').on('click touch', function(e) {
    $(this).parent().parent().prev('a').addClass('child--active')
 });
