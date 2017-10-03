18e165b47f902cb9ac478231c09c3ba896d8ea1f
rkingston <r.kingston@northeastern.edu>
2017-10-03 16:18:08 -0400

    nav update

--- docs/views/parts/navs.php
+++ docs/views/parts/navs.php
@@ -72,42 +72,108 @@
             </ul>
           </li>
         </ul>
-        <div class="tab-content">
-          <div class="tab-pane active" id="tab_1" role="tabpanel">
-            <h3>Content 1</h3>
-            <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
-          <div class="tab-pane" id="tab_2" role="tabpanel">
-            <h3>Content 2</h3>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur.</p>
-          </div>
-          <div class="tab-pane" id="tab_3" role="tabpanel">
-            <h3>Content 3</h3>
-            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
-          </div>
-          <div class="tab-pane pt--2@md" id="tab_4" role="tabpanel">
-            <h3>Content 4</h3>
-            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor. Donec id elit non mi porta gravida at eget metus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
-          </div>
-          <div class="tab-pane pt--2@md" id="tab_4a" role="tabpanel">
-            <h3>Content 4a</h3>
-            <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
-          </div>
-          <div class="tab-pane pt--2@md" id="tab_4b" role="tabpanel">
-            <h3>Content 4b</h3>
-            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. Etiam porta sem malesuada magna mollis euismod.</p>
-          </div>
-          <div class="tab-pane pt--2@md" id="tab_4c" role="tabpanel">
-            <h3>Content 4c</h3>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Etiam porta sem malesuada magna mollis euismod. Maecenas faucibus mollis interdum. Donec sed odio dui. Curabitur blandit tempus porttitor. Sed posuere consectetur est at lobortis. Aenean lacinia bibendum nulla sed consectetur.</p>
-          </div>
-        </div>
       </nav>
+      <div class="hidden pt--1@xs active" id="tab_1" role="tabpanel">
+        <h3>Content 1</h3>
+        <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <div class="hidden pt--1@xs" id="tab_2" role="tabpanel">
+        <h3>Content 2</h3>
+        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur.</p>
+      </div>
+      <div class="hidden pt--1@xs" id="tab_3" role="tabpanel">
+        <h3>Content 3</h3>
+        <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+      </div>
+      <div class="hidden pt--3@md" id="tab_4" role="tabpanel">
+        <h3>Content 4</h3>
+        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor. Donec id elit non mi porta gravida at eget metus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+      </div>
+      <div class="hidden pt--3@md" id="tab_4a" role="tabpanel">
+        <h3>Content 4a</h3>
+        <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+      </div>
+      <div class="hidden pt--3@md" id="tab_4b" role="tabpanel">
+        <h3>Content 4b</h3>
+        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. Etiam porta sem malesuada magna mollis euismod.</p>
+      </div>
+      <div class="hidden pt--3@md" id="tab_4c" role="tabpanel">
+        <h3>Content 4c</h3>
+        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Etiam porta sem malesuada magna mollis euismod. Maecenas faucibus mollis interdum. Donec sed odio dui. Curabitur blandit tempus porttitor. Sed posuere consectetur est at lobortis. Aenean lacinia bibendum nulla sed consectetur.</p>
+      </div>
     </div>
   </div>
 </section>
 
 <section class="section">
+  <header class="k_section__header" style="border-bottom: 0 none;">Tabbed Nav: Bordered</header>
+  <div class="row">
+    <div class="col --12@xs">
+      <nav class="nav --tabbed --bordered">
+        <ul class="__list" role="tablist">
+          <li class="__item">
+            <a class="__link active" data-toggle="tab" href="#tab_5" role="tab" aria-expanded="true">Mozzarella Sticks</a>
+          </li>
+          <li class="__item">
+            <a class="__link" data-toggle="tab" href="#tab_6" role="tab" aria-expanded="false">Mushroom Pizza Pie</a>
+          </li>
+          <li class="__item">
+            <a class="__link" data-toggle="tab" href="#tab_7" role="tab" aria-expanded="false">Fried Ice Cream</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="hidden pt--1@xs active" id="tab_5" role="tabpanel">
+        <h3>Mozzarella Sticks</h3>
+        <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <div class="hidden pt--1@xs" id="tab_6" role="tabpanel">
+        <h3>Mushroom Pizza Pie</h3>
+        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur.</p>
+      </div>
+      <div class="hidden pt--1@xs" id="tab_7" role="tabpanel">
+        <h3>Fried Ice Cream</h3>
+        <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="section">
+  <header class="k_section__header">Tabbed Nav: Buttons</header>
+  <div class="row">
+    <div class="col --12@xs">
+      <p>Button Tabs should default to no active selection, requiring engagement to reveal content, as if targeting differing content to a specific audience group.
+      <p><b>Information for:</b></p>
+      <nav class="nav --tabbed --buttons">
+        <ul class="__list" role="tablist">
+          <li class="__item">
+            <a class="__link --color-1" data-toggle="tab" href="#tab_8" role="tab" aria-expanded="true">Undergraduates</a>
+          </li>
+          <li class="__item">
+            <a class="__link --color-2" data-toggle="tab" href="#tab_9" role="tab" aria-expanded="false">Graduate Students</a>
+          </li>
+          <li class="__item">
+            <a class="__link --color-3" data-toggle="tab" href="#tab_10" role="tab" aria-expanded="false">Alumni</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="hidden pt--1@xs pt--1@xs" id="tab_8" role="tabpanel">
+        <h3>Undergraduate Students</h3>
+        <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <div class="hidden pt--1@xs" id="tab_9" role="tabpanel">
+        <h3>Graduate Students</h3>
+        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur.</p>
+      </div>
+      <div class="hidden pt--1@xs" id="tab_10" role="tabpanel">
+        <h3>Alumni</h3>
+        <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="section k_bg-light">
   <header class="k_section__header mb--2@xs">Pagination</header>
   <nav class="nav --pagination" aria-label="Page navigation">
     <ul class="__list">
