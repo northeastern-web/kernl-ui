dd92fb9aa460ff1e3482c887f262540e6170b18e
Tim Albert <timothy.m.albert@gmail.com>
2017-10-19 16:24:44 -0400

    Update modal a bit

--- docs/views/partials/_modal-search.php
+++ docs/views/partials/_modal-search.php
@@ -1,14 +1,28 @@
 <div class="modal fade --dark --search" id="modal_search" tabindex="-1" role="dialog" aria-labelledby="modal_search_label" aria-hidden="true">
+  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
   <div class="modal-dialog __dialog" role="document">
     <div class="__content">
-      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
       <div class="__body">
         <form action="" method="">
           <div class="form__group">
             <label for="exampleText" class="sr--only">Search</label>
             <input type="search" class="form__control mt--2@xs" id="exampleText" aria-describedby="textHelp" placeholder="Well, lookie here...">
+          </div>
+          <div class="form__group">
+            <div class="form__check form__check--inline">
+              <label class="form__check__label">
+                <input type="radio" name="opt" class="form__check__input">
+                Check me out
+              </label>
+            </div>
+            <div class="form__check form__check--inline">
+              <label class="form__check__label">
+                <input type="radio" name="opt" class="form__check__input">
+                Check me out
+              </label>
+            </div>
           </div>
         </form>
       </div>
