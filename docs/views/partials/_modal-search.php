eedb91aa12583c966f7030cdda14f2c8b09b8263
Tim Albert <timothy.m.albert@gmail.com>
2017-10-27 16:43:55 -0400

    Update modal search color

--- docs/views/partials/_modal-search.php
+++ docs/views/partials/_modal-search.php
@@ -6,20 +6,20 @@
     <div class="__content">
       <div class="__body">
         <form action="" method="">
-          <div class="form__group">
+          <div class="__group">
             <label for="exampleText" class="sr--only">Search</label>
-            <input type="search" class="form__control mt--2@xs" id="exampleText" aria-describedby="textHelp" placeholder="Well, lookie here...">
+            <input type="search" class="__control mt--2@xs" id="exampleText" aria-describedby="textHelp" placeholder="Well, lookie here...">
           </div>
-          <div class="form__group">
-            <div class="form__check form__check--inline">
-              <label class="form__check__label">
-                <input type="radio" name="opt" class="form__check__input">
+          <div class="__group">
+            <div class="__check __check--inline">
+              <label class="__check__label">
+                <input type="radio" name="opt" class="__check__input">
                 Check me out
               </label>
             </div>
-            <div class="form__check form__check--inline">
-              <label class="form__check__label">
-                <input type="radio" name="opt" class="form__check__input">
+            <div class="__check __check--inline">
+              <label class="__check__label">
+                <input type="radio" name="opt" class="__check__input">
                 Check me out
               </label>
             </div>
