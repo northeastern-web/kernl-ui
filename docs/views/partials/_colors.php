95fdd00382c98f03b50a4b1e1359ed45636b691f
rkingston <r.kingston@northeastern.edu>
2017-12-20 13:13:20 -0500

    refactor button styles

--- docs/views/partials/_colors.php
+++ docs/views/partials/_colors.php
@@ -6,7 +6,7 @@ function colorPalette($color, $colorName) {
       <div class="k_color bg--' . $color . ' py--2@xs">
         <h3>' . $colorName . ' Background</h3>
         <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
-        <a class="button --sm --pill" href="#">More</a> <a class="button --sm --pill" href="#">Less</a>
+        <a class="btn --sm --pill" href="#">More</a> <a class="btn --sm --pill" href="#">Less</a>
       </div>
       <div class="alert --sm bg--' . $color . '">
         <a class="__link" href="#">
@@ -18,14 +18,14 @@ function colorPalette($color, $colorName) {
       <div class="k_color text--' . $color . ' pt--0@xs pl--0@xs">
         <h5>' . $colorName . ' Foreground</h5>
         <p class="fs--sm mb--2@xs">Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
-        <a class="button bg--' . $color . ' --' . $color . ' --sm --pill" href="#">More</a> <a class="button border--' . $color . ' --' . $color . ' --outline --sm --pill" href="#">Less</a>
+        <a class="btn bg--' . $color . ' --' . $color . ' --sm --pill" href="#">More</a> <a class="btn border--' . $color . ' --' . $color . ' --outline --sm --pill" href="#">Less</a>
       </div>
     </div>
     <div class="col --12@xs --6@md">
       <div class="k_color border--' . $color . ' mb--4@xs">
         <h5>' . $colorName . ' Border</h5>
         <p class="fs--sm">Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
-        <a class="button bg--' . $color . ' --xs --' . $color . ' --pill" href="#">More</a> <a class="button border--' . $color . ' --xs --' . $color . ' --outline --pill" href="#">Less</a>
+        <a class="btn bg--' . $color . ' --xs --' . $color . ' --pill" href="#">More</a> <a class="btn border--' . $color . ' --xs --' . $color . ' --outline --pill" href="#">Less</a>
       </div>
     </div>
   </div>
--- docs/views/parts/buttons.php
+++ docs/views/parts/buttons.php
@@ -11,19 +11,19 @@
     </div>
     <div class="col --12@xs --4@md">
       <p class="text-style-block">(Semi) Transparent</p>
-      <p><a class="btn --transparent --xs" data-component="button" href="#">Transparent xs</a></p>
-      <p><a class="btn --transparent --sm" data-component="button" href="#">Transparent sm</a></p>
-      <p><a class="btn --transparent --pill" data-component="button" href="#">Pill Transparent</a></p>
-      <p><a class="btn --transparent --lg" data-component="button" href="#">Transparent lg</a></p>
-      <p><a class="btn --transparent --lg --block" data-component="button" href="#">Block Transparent lg</a></p>
+      <p><a class="btn bg--transparent-dark --xs" data-component="button" href="#">Transparent xs</a></p>
+      <p><a class="btn bg--transparent-dark --sm" data-component="button" href="#">Transparent sm</a></p>
+      <p><a class="btn bg--transparent-dark --pill" data-component="button" href="#">Pill Transparent</a></p>
+      <p><a class="btn bg--transparent-dark --lg" data-component="button" href="#">Transparent lg</a></p>
+      <p><a class="btn bg--transparent-dark --lg --block" data-component="button" href="#">Block Transparent lg</a></p>
     </div>
     <div class="col --12@xs --4@md">
       <p class="text-style-block">Dark Buttons</p>
-      <p><a class="btn --dark --xs" data-component="button" href="#">Dark xs</a></p>
-      <p><a class="btn --dark --sm" data-component="button" href="#">Dark sm</a></p>
-      <p><a class="btn --dark --pill" data-component="button" href="#">Pill Dark</a></p>
-      <p><a class="btn --dark --lg" data-component="button" href="#">Dark lg</a></p>
-      <p><a class="btn --dark --lg --block" data-component="button" href="#">Block Dark lg</a></p>
+      <p><a class="btn bg--black --xs" data-component="button" href="#">Dark xs</a></p>
+      <p><a class="btn bg--black --sm" data-component="button" href="#">Dark sm</a></p>
+      <p><a class="btn bg--black --pill" data-component="button" href="#">Pill Dark</a></p>
+      <p><a class="btn bg--black --lg" data-component="button" href="#">Dark lg</a></p>
+      <p><a class="btn bg--black --lg --block" data-component="button" href="#">Block Dark lg</a></p>
     </div>
   </div>
 </section>
@@ -40,27 +40,27 @@
     </div>
     <div class="col --12@xs --6@md --3@xl">
       <p class="text-style-block">(Semi) Transparent</p>
-      <p><a class="btn --transparent --xs" data-component="button" href="#">Transparent xs</a></p>
-      <p><a class="btn --transparent --sm" data-component="button" href="#">Transparent sm</a></p>
-      <p><a class="btn --transparent --pill" data-component="button" href="#">Pill Transparent</a></p>
-      <p><a class="btn --transparent --lg" data-component="button" href="#">Transparent lg</a></p>
-      <p><a class="btn --transparent --lg --block" data-component="button" href="#">Block Transparent lg</a></p>
+      <p><a class="btn bg--transparent-dark --xs" data-component="button" href="#">Transparent xs</a></p>
+      <p><a class="btn bg--transparent-dark --sm" data-component="button" href="#">Transparent sm</a></p>
+      <p><a class="btn bg--transparent-dark --pill" data-component="button" href="#">Pill Transparent</a></p>
+      <p><a class="btn bg--transparent-dark --lg" data-component="button" href="#">Transparent lg</a></p>
+      <p><a class="btn bg--transparent-dark --lg --block" data-component="button" href="#">Block Transparent lg</a></p>
     </div>
     <div class="col --12@xs --6@md --3@xl">
       <p class="text-style-block">(Semi) Transparent Light</p>
-      <p><a class="btn --transparent --light --xs" data-component="button" href="#">Transparent Light xs</a></p>
-      <p><a class="btn --transparent --light --sm" data-component="button" href="#">Transparent Light sm</a></p>
-      <p><a class="btn --transparent --light --pill" data-component="button" href="#">Pill Transparent Light</a></p>
-      <p><a class="btn --transparent --light --lg" data-component="button" href="#">Transparent Light lg</a></p>
-      <p><a class="btn --transparent --light --lg --block" data-component="button" href="#">Block Transparent Light lg</a></p>
+      <p><a class="btn bg--transparent-light --xs" data-component="button" href="#">Transparent Light xs</a></p>
+      <p><a class="btn bg--transparent-light --sm" data-component="button" href="#">Transparent Light sm</a></p>
+      <p><a class="btn bg--transparent-light --pill" data-component="button" href="#">Pill Transparent Light</a></p>
+      <p><a class="btn bg--transparent-light --lg" data-component="button" href="#">Transparent Light lg</a></p>
+      <p><a class="btn bg--transparent-light --lg --block" data-component="button" href="#">Block Transparent Light lg</a></p>
     </div>
     <div class="col --12@xs --6@md --3@xl">
-      <p class="text-style-block">White Outline Buttons</p>
-      <p><a class="btn --white --xs --outline" data-component="button" href="#">White Outline xs</a></p>
-      <p><a class="btn --white --sm --outline" data-component="button" href="#">White Outline sm</a></p>
-      <p><a class="btn --white --pill --outline" data-component="button" href="#">Pill White Outline</a></p>
-      <p><a class="btn --white --lg --outline" data-component="button" href="#">White Outline lg</a></p>
-      <p><a class="btn --white --lg --block --outline" data-component="button" href="#">Block White Outline lg</a></p>
+      <p class="text-style-block">White Border Buttons</p>
+      <p><a class="btn border--white --xs" data-component="button" href="#">White Border xs</a></p>
+      <p><a class="btn border--white --sm" data-component="button" href="#">White Border sm</a></p>
+      <p><a class="btn border--white --pill" data-component="button" href="#">Pill White Border</a></p>
+      <p><a class="btn border--white --lg" data-component="button" href="#">White Border lg</a></p>
+      <p><a class="btn border--white --lg --block" data-component="button" href="#">Block White Border lg</a></p>
     </div>
   </div>
 </section>
@@ -68,20 +68,20 @@
   <header class="k_section__header ta--l">Buttons in Color</header>
   <div class="row">
     <div class="col --12@xs --3@md">
-      <p><a class="btn --gray-500" data-component="button" href="#">Gray-500 Button</a></p>
-      <p><a class="btn --gray-500 --outline" data-component="button" href="#">Gray-500 Button Outline</a></p>
+      <p><a class="btn bg--gray-500" data-component="button" href="#">Gray-500 Button</a></p>
+      <p><a class="btn border--gray-500" data-component="button" href="#">Gray-500 Button Border</a></p>
     </div>
     <div class="col --12@xs --3@md">
-      <p><a class="btn --red" data-component="button" href="#">Red Button</a></p>
-      <p><a class="btn --red --outline" data-component="button" href="#">Red Button Outline</a></p>
+      <p><a class="btn bg--red" data-component="button" href="#">Red Button</a></p>
+      <p><a class="btn border--red" data-component="button" href="#">Red Button Border</a></p>
     </div>
     <div class="col --12@xs --3@md">
-      <p><a class="btn --blue" data-component="button" href="#">Blue Button</a></p>
-      <p><a class="btn --blue --outline" data-component="button" href="#">Blue Button Outline</a></p>
+      <p><a class="btn bg--blue" data-component="button" href="#">Blue Button</a></p>
+      <p><a class="btn border--blue" data-component="button" href="#">Blue Button Border</a></p>
     </div>
     <div class="col --12@xs --3@md">
-      <p><a class="btn --black" data-component="button" href="#">Black Button</a></p>
-      <p><a class="btn --black --outline" data-component="button" href="#">Black Button Outline</a></p>
+      <p><a class="btn bg--black" data-component="button" href="#">Black Button</a></p>
+      <p><a class="btn border--black" data-component="button" href="#">Black Button Border</a></p>
     </div>
   </div>
 </section>
@@ -105,7 +105,7 @@
         <small><i>Add <code>.btn_group</code> to a button wrapper or href wrapper.</i></small>
       </p>
       <p class="btn__group">
-        <button class="btn --sm --dark" data-component="button">This One</button>
+        <button class="btn bg--gray-800 --sm" data-component="button">This One</button>
         <button class="btn --sm" data-component="button">That One</button>
         <button class="btn --sm" data-component="button" disabled="disabled">Not This One</button>
       </p>
@@ -139,7 +139,7 @@
         </a>
       </p>
       <p>
-        <a class="btn --lg --transparent" data-component="button" href="#">
+        <a class="btn --lg bg--transparent-dark" data-component="button" href="#">
           <span class="__icon --lg --thin"><i data-feather="cloud-rain"></i></span><br>
           Looks Like We're In For<br>
           Some Nasty Weather
