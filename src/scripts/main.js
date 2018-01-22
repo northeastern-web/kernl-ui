86ef73a9d463bcd9a9a76c3a094b623b3d00b3bf
Tim Albert <timothy.m.albert@gmail.com>
2018-01-22 16:19:16 -0500

    Update with latest Bootstrap scripts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,11 @@
+// Bootstrap v4
 import './bootstrap/index'
-import './components/mastheads.js'
-import './components/navs.js'
-import './components/sharing.js'
 
+// Kernl(UI) items
+import './components/mastheads'
+import './components/navs'
+import './components/sharing'
+
+// Feather icons
 import Feather from 'feather-icons'
-
 Feather.replace()
