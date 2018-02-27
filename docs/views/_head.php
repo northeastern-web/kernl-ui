1c49c875a401ba29bd26a31391664553e17a4ad5
rkingston <r.kingston@northeastern.edu>
2018-02-27 12:45:08 -0500

    update nav style (sizes and active)

--- docs/views/_head.php
+++ docs/views/_head.php
@@ -3,6 +3,6 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>kernl(ui)</title>
-  <link href='//fonts.googleapis.com/css?family=Lato:300,300italic,400,400italic,700,700italic,900|Zilla+Slab:400,400i,700,700i|Changa:500|Source+Code+Pro' rel='stylesheet' type='text/css'>
+  <link href='//fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i,900|Roboto+Slab:300,400,700|Changa:500|Source+Code+Pro' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="/dist/styles/main.css">
 </head>
