74fcd3f697829558e4dcbdba951389baebcbe17a
rkingston <r.kingston@northeastern.edu>
2018-03-01 10:45:51 -0500

    update class name for nav toggler

--- src/scripts/components/navs.js
+++ src/scripts/components/navs.js
@@ -4,6 +4,6 @@
 
 
 // On-screen nav toggle for small screens
-$('.nav__toggler').on('click touch', function(e) {
+$('.__toggler').on('click touch', function(e) {
   $('body').toggleClass('nav--open')
 });
