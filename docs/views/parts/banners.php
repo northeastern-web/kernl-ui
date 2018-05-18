eacfdc85041b0f756ab507892a55040fb0eec7c8
rkingston <r.kingston@northeastern.edu>
2018-05-18 14:13:37 -0400

    change banner to vh units; nav tweaks

--- docs/views/parts/banners.php
+++ docs/views/parts/banners.php
@@ -5,59 +5,79 @@
 </header>
 
 <section class="section --banner mb--1@xs mb--2@lg">
-  <h1 class="__title">Default Section</h1>
+  <div class="__body">
+    <h1 class="__title">Default Section</h1>
+  </div>
 </section>
 
-<section class="section --banner --md +bgimg mb--1@xs mb--2@lg">
-  <h1 class="__title">Medium Section with Image</h1>
-  <div class="__subtitle">Includes Background Image</div>
+<section class="section --banner --50 +bgimg mb--1@xs mb--2@lg">
+  <div class="__body">
+    <h1 class="__title">Medium Section with Image</h1>
+    <div class="__subtitle">Includes Background Image</div>
+  </div>
 </section>
 
-<section class="section --banner +bgimg mb--1@xs mb--2@lg">
-  <h1 class="__title">Default Section with Image</h1>
-  <div class="__subtitle">Includes Background Image</div>
+<section class="section --banner --100 +bgimg mb--1@xs mb--2@lg">
+  <div class="__body">
+    <h1 class="__title">Default Section with Image</h1>
+    <div class="__subtitle">Includes Background Image</div>
+  </div>
 </section>
 
-<section class="section --banner --lg +bgimg mb--1@xs mb--2@lg">
-  <div class="__pretitle">Header Pre Title</div>
-  <h1 class="__title">Header: Large</h1>
-  <div class="__subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
+<section class="section --banner --70 +bgimg mb--1@xs mb--2@lg">
+  <div class="__body">
+    <div class="__pretitle">Header Pre Title</div>
+    <h1 class="__title">Header: Large</h1>
+    <div class="__subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
+  </div>
 </section>
 
-<section class="section --banner --xl +bgimg mb--1@xs mb--2@lg">
-  <div class="__pretitle">Header Pre Title</div>
-  <h1 class="__title">Header: Extra Large</h1>
-  <div class="__subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
+<section class="section --banner --100 +bgimg mb--1@xs mb--2@lg">
+  <div class="__body">
+    <div class="__pretitle">Header Pre Title</div>
+    <h1 class="__title">Header: Extra Large</h1>
+    <div class="__subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
+  </div>
 </section>
 
-<section class="section --banner --lg --bottom +bgimg mb--1@xs mb--2@lg">
-  <div class="__pretitle">Header Pre Title</div>
-  <h1 class="__title">Header: Large, Bottom</h1>
-  <div class="__subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
+<section class="section --banner --80 --bottom +bgimg mb--1@xs mb--2@lg">
+  <div class="__body">
+    <div class="__pretitle">Header Pre Title</div>
+    <h1 class="__title">Header: Large, Bottom</h1>
+    <div class="__subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
+  </div>
 </section>
 
 <header class="section k_bg-light pt--2@xs">
   <h2 class="k_section__header">Headers, Fancy Options</h2>
 </header>
 
-<section class="section --banner bg--fixed --lg +bgimg mb--1@xs mb--2@lg">
-  <div class="__pretitle">Header Pre Title</div>
-  <h1 class="__title">Header: Large, Fixed</h1>
-  <div class="__subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
+<section class="section --banner bg--fixed --60 +bgimg mb--1@xs mb--2@lg">
+  <div class="__body">
+    <div class="__pretitle">Header Pre Title</div>
+    <h1 class="__title">Header: Large, Fixed</h1>
+    <div class="__subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
+  </div>
 </section>
 
 <section class="section --banner bg--gray-700 mb--1@xs mb--2@lg">
-  <div class="section --lg +bgimg px--1@xs px--2@md">
-    <div class="__pretitle">Header Pre Title</div>
-    <h1 class="__title">Header, Contained: Large</h1>
-    <div class="__subtitle">This section is <i>inside</i> a parent banner section.</div>
+  <div class="__body">
+    <div class="section --lg +bgimg px--1@xs px--2@md">
+      <div class="__pretitle">Header Pre Title</div>
+      <h1 class="__title">Header, Contained: Large</h1>
+      <div class="__subtitle">This section is <i>inside</i> a parent banner section.</div>
+    </div>
   </div>
 </section>
 
 <section class="section --banner --transparent +shadow mb--1@xs mb--2@lg">
-  <h1 class="__title">Header: Transparent, Shadow</h1>
+  <div class="__body">
+    <h1 class="__title">Header: Transparent, Shadow</h1>
+  </div>
 </section>
 
 <section class="section --banner --white mb--1@xs mb--2@lg">
-  <h1 class="__title">Header: White</h1>
+  <div class="__body">
+    <h1 class="__title">Header: White</h1>
+  </div>
 </section>
