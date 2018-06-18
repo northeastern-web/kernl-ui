253ba316789fed1624a0441028e2663a2ee182e5
Tim Albert <timothy.m.albert@gmail.com>
2018-06-18 10:59:28 -0400

    Buttons first stab

--- docs/views/parts/buttons.php
+++ docs/views/parts/buttons.php
@@ -3,27 +3,18 @@
   <div class="row">
     <div class="col --12@xs --4@md">
       <p class="text-style-block">Base Buttons</p>
-      <p><a class="btn --xs" data-component="button" href="#">Button xs</a></p>
       <p><a class="btn --sm --pill" data-component="button" href="#">Button Pill sm</a></p>
       <p><a class="btn" data-component="button" href="#">Base Button</a></p>
-      <p><a class="btn --lg" data-component="button" href="#">Button lg</a></p>
-      <p><a class="btn --block --lg" data-component="button" href="#">Button block lg</a></p>
     </div>
     <div class="col --12@xs --4@md">
       <p class="text-style-block">(Semi) Transparent</p>
-      <p><a class="btn bg--transparent-dark --xs" data-component="button" href="#">Transparent xs</a></p>
       <p><a class="btn bg--transparent-dark --sm" data-component="button" href="#">Transparent sm</a></p>
       <p><a class="btn bg--transparent-dark --pill" data-component="button" href="#">Pill Transparent</a></p>
-      <p><a class="btn bg--transparent-dark --lg" data-component="button" href="#">Transparent lg</a></p>
-      <p><a class="btn bg--transparent-dark --lg --block" data-component="button" href="#">Block Transparent lg</a></p>
     </div>
     <div class="col --12@xs --4@md">
       <p class="text-style-block">Dark Buttons</p>
-      <p><a class="btn bg--black --xs" data-component="button" href="#">Dark xs</a></p>
       <p><a class="btn bg--black --sm" data-component="button" href="#">Dark sm</a></p>
       <p><a class="btn bg--black --pill" data-component="button" href="#">Pill Dark</a></p>
-      <p><a class="btn bg--black --lg" data-component="button" href="#">Dark lg</a></p>
-      <p><a class="btn bg--black --lg --block" data-component="button" href="#">Block Dark lg</a></p>
     </div>
   </div>
 </section>
@@ -32,35 +23,23 @@
   <div class="row">
     <div class="col --12@xs --6@md --3@xl">
       <p class="text-style-block">Base Buttons</p>
-      <p><a class="btn --xs" data-component="button" href="#">Button xs</a></p>
       <p><a class="btn --sm --pill" data-component="button" href="#">Button Pill sm</a></p>
       <p><a class="btn" data-component="button" href="#">Base Button</a></p>
-      <p><a class="btn --lg" data-component="button" href="#">Button lg</a></p>
-      <p><a class="btn --lg --block" data-component="button" href="#">Button block lg</a></p>
     </div>
     <div class="col --12@xs --6@md --3@xl">
       <p class="text-style-block">(Semi) Transparent</p>
-      <p><a class="btn bg--transparent-dark --xs" data-component="button" href="#">Transparent xs</a></p>
       <p><a class="btn bg--transparent-dark --sm" data-component="button" href="#">Transparent sm</a></p>
       <p><a class="btn bg--transparent-dark --pill" data-component="button" href="#">Pill Transparent</a></p>
-      <p><a class="btn bg--transparent-dark --lg" data-component="button" href="#">Transparent lg</a></p>
-      <p><a class="btn bg--transparent-dark --lg --block" data-component="button" href="#">Block Transparent lg</a></p>
     </div>
     <div class="col --12@xs --6@md --3@xl">
       <p class="text-style-block">(Semi) Transparent Light</p>
-      <p><a class="btn bg--transparent-light --xs" data-component="button" href="#">Transparent Light xs</a></p>
       <p><a class="btn bg--transparent-light --sm" data-component="button" href="#">Transparent Light sm</a></p>
       <p><a class="btn bg--transparent-light --pill" data-component="button" href="#">Pill Transparent Light</a></p>
-      <p><a class="btn bg--transparent-light --lg" data-component="button" href="#">Transparent Light lg</a></p>
-      <p><a class="btn bg--transparent-light --lg --block" data-component="button" href="#">Block Transparent Light lg</a></p>
     </div>
     <div class="col --12@xs --6@md --3@xl">
       <p class="text-style-block">White Border Buttons</p>
-      <p><a class="btn bc--white --xs" data-component="button" href="#">White Border xs</a></p>
       <p><a class="btn bc--white --sm" data-component="button" href="#">White Border sm</a></p>
       <p><a class="btn bc--white --pill" data-component="button" href="#">Pill White Border</a></p>
-      <p><a class="btn bc--yellow bg--none --lg" data-component="button" href="#">White Border lg</a></p>
-      <p><a class="btn bc--white --lg --block" data-component="button" href="#">Block White Border lg</a></p>
     </div>
   </div>
 </section>
@@ -114,12 +93,6 @@
       <p>
         <span class="text-style-block">With Iconography</span><br>
         <small><i>Add <code>__icon</code> class to Feather Icons.<br>
-        Define size and position with <code>--left</code>, <code>--right</code>, <code>--lg</code>, <code>--thin</code>.</i></small>
-      </p>
-      <p>
-        <a class="btn --xs" data-component="button" href="#">Go Forth
-          <i class="__icon --right" data-feather="fast-forward"></i>
-        </a>
       </p>
       <p>
         <a class="btn --sm" data-component="button" href="#">
@@ -129,32 +102,15 @@
       </p>
       <p>
         <a class="btn" data-component="button" href="#">
-          <i class="__icon --left --lg --thin" data-feather="headphones"></i>
           Hear Me Now and<br>
           Listen to Me Later
         </a>
       </p>
       <p>
         <a class="btn" data-component="button" href="#">
-          <i class="__icon --left --lg --thin" data-feather="headphones"></i>
           Hear Me Now and Listen to Me Later<br>
           Listen to Me Later Hear Me Now and Listen to Me Later. Hear Me Now<br>
           Listen to Me Later
-        </a>
-      </p>
-      <p>
-        <a class="btn --lg" data-component="button" href="#">Now Fly Away
-          <i class="__icon --right" data-feather="feather"></i>
-        </a>
-      </p>
-      <p>
-        <small><i>Add <code>.flex--col@&lt;breakpoint&gt;</code> to <code>.btn</code> to stack the icon and text.</i></small>
-      </p>
-      <p>
-        <a class="btn --lg bg--transparent-dark flex--col@xs" data-component="button" href="#">
-          <i class="__icon --lg --thin" data-feather="cloud-rain"></i>
-          Looks Like We're In For<br>
-          Some Nasty Weather
         </a>
       </p>
     </div>
