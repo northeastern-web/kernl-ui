5809c49ca0ef025dc16bc8c2a51c813f5c7c1e0b
rkingston <r.kingston@northeastern.edu>
2018-06-25 17:08:56 -0400

    columns/grid docs tweaks

--- docs/views/parts/columns.php
+++ docs/views/parts/columns.php
@@ -10,7 +10,7 @@
     </div>
   </section>
   <section class="section k_grid-lines k_10-lines">
-    <div class="row">
+    <div class="row pt--2">
       <div class="col w--20@t w--10@d">
         <div class="col-content">w--20@t w--10@d</div>
       </div>
@@ -61,7 +61,7 @@
     </div>
   </section>
   <section class="section k_grid-lines k_6-lines">
-    <div class="row">
+    <div class="row pt--2">
       <div class="col w--1/6">
         <div class="col-content">w--1/6</div>
       </div>
@@ -91,7 +91,7 @@
     </div>
   </section>
   <section class="section k_grid-lines k_4-lines">
-    <div class="row">
+    <div class="row pt--2">
       <div class="col w--1/4">
         <div class="col-content">w--1/4</div>
       </div>
@@ -124,22 +124,34 @@
   </section>
   <section class="section">
     <div class="row">
+      <div class="col w--auto">
+        <div class="col-content">w--auto (no specific width set; width defined by content)</div>
+      </div>
+      <div class="col w--fit">
+        <div class="col-content">w--fit</div>
+      </div>
+    </div>
+    <div class="row">
       <div class="col w--3/4@d">
         <div class="col-content">
-          <p>9</p>
+          <p>w--3/4@d</p>
           <p>Nulla vitae elit libero, a pharetra augue. Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Vestibulum id ligula porta felis euismod semper. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec id elit non mi porta gravida at eget metus. Nulla vitae elit libero, a pharetra augue.</p>
           <p>Cras mattis consectetur purus sit amet fermentum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
         </div>
       </div>
       <div class="col w--1/4@d">
         <div class="col-content pos--sticky">
-          3. Sticky
+          w--3/4@d, Sticky
         </div>
       </div>
     </div>
   </section>
 
   <hr>
+
+  <section class="section">
+    <header class="k_section__header">Grid, No Gutters</header>
+  </section>
 
   <section class="section --nogutters">
     <div class="row">
@@ -260,147 +272,27 @@
 
   <section class="section">
     <header class="k_section__header">Grid, Banners, Nesting, Fancy Options and Tweaks</header>
-    <h3 class="non-col-element text-style-block">Section</h3>
-    <div class="row">
-      <div class="col --1@md">
-        <div class="col-content">1</div>
-      </div>
-      <div class="col --1@md">
-        <div class="col-content">1</div>
-      </div>
-      <div class="col --1@md">
-        <div class="col-content">1</div>
-      </div>
-      <div class="col --1@md">
-        <div class="col-content">1</div>
-      </div>
-      <div class="col --1@md">
-        <div class="col-content">1</div>
-      </div>
-      <div class="col --1@md">
-        <div class="col-content">1</div>
-      </div>
-      <div class="col --1@md">
-        <div class="col-content">1</div>
-      </div>
-      <div class="col --1@md">
-        <div class="col-content">1</div>
-      </div>
-      <div class="col --1@md">
-        <div class="col-content">1</div>
-      </div>
-      <div class="col --1@md">
-        <div class="col-content">1</div>
-      </div>
-      <div class="col --1@md">
-        <div class="col-content">1</div>
-      </div>
-      <div class="col --1@md">
-        <div class="col-content">1</div>
-      </div>
-    </div>
-  </section>
-
-  <hr>
-
-  <section class="section bg--gray-700 tc--gray-100">
-    <h3 class="non-col-element text-style-block">Section <small>(with --bg-color option)</small></h3>
-    <div class="row">
-      <div class="col --2@md">
-        <div class="col-content">2</div>
-      </div>
-      <div class="col --2@md">
-        <div class="col-content">2</div>
-      </div>
-      <div class="col --2@md">
-        <div class="col-content">2</div>
-      </div>
-      <div class="col --2@md">
-        <div class="col-content">2</div>
-      </div>
-      <div class="col --2@md">
-        <div class="col-content">2</div>
-      </div>
-      <div class="col --2@md">
-        <div class="col-content">2</div>
-      </div>
-    </div>
   </section>
   <section class="section">
-    <h3 class="non-col-element text-style-block">Section</h3>
-    <div class="row">
-      <div class="col --3@md">
-        <div class="col-content">3</div>
-      </div>
-      <div class="col --3@md">
-        <div class="col-content">3</div>
-      </div>
-      <div class="col --3@md">
-        <div class="col-content">3</div>
-      </div>
-      <div class="col --3@md">
-        <div class="col-content">3</div>
-      </div>
-    </div>
-  </section>
-  <section class="section bg--gray-700 tc--gray-100">
     <h3 class="non-col-element text-style-block">Section <small>(with --bg-color option)</small></h3>
-    <div class="row">
-      <div class="col --4@md">
-        <div class="col-content">4</div>
-      </div>
-      <div class="col --4@md">
-        <div class="col-content">4</div>
-      </div>
-      <div class="col --4@md">
-        <div class="col-content">4</div>
-      </div>
-    </div>
-  </section>
-  <section class="section">
-    <h3 class="non-col-element text-style-block">Section</h3>
-    <div class="row">
-      <div class="col --5@md">
-        <div class="col-content">5</div>
-      </div>
-      <div class="col --7@md">
-        <div class="col-content">7</div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col --6@md">
-        <div class="col-content">6</div>
-      </div>
-      <div class="col --6@md">
-        <div class="col-content">6</div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col --7@md">
-        <div class="col-content">7</div>
-      </div>
-      <div class="col --5@md">
-        <div class="col-content">5</div>
-      </div>
-    </div>
     <div class="row +middle">
-      <div class="col --8@md">
-        <section class="section bg--gray-700 tc--gray-100 pa--1@xs">
+      <div class="col w--2/3@t">
+        <section class="section bg--gray-700 tc--gray-100 pa--1">
           <h3 class="non-col-element text-style-block">Nested Section <small>(with --bg-color option)</small></h3>
           <div class="row">
-            <div class="col --4@md">
+            <div class="col w--1/3@t">
               <div class="col-content">4<br>----</div>
             </div>
-            <div class="col --4@md">
+            <div class="col w--1/3@t">
               <div class="col-content">4<br>----<br>-------<br>-----</div>
             </div>
-            <div class="col --4@md">
+            <div class="col w--1/3@t">
               <div class="col-content">4<br>----<br>-------</div>
             </div>
           </div>
         </section>
       </div>
-      <div class="col --4@md">
+      <div class="col w--1/3@t">
         <div class="col-content">4 (+middle on row)</div>
       </div>
     </div>
