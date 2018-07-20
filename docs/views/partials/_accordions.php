5a4d99c6dfd571686d2eff1d34bd2ec5c30b97e1
rkingston <r.kingston@northeastern.edu>
2018-07-20 11:50:42 -0400

    working formstone accordion

--- docs/views/partials/_accordions.php
+++ docs/views/partials/_accordions.php
@@ -16,13 +16,13 @@ function accordion($id, $variant, $section_classes, $acc_class) {
         <header class="k_section__header">' . $variant . ' Accordion</header>
         <div class="accordion ' . $acc_class . '">
           <div class="__item">
-            <div class="__title --active"
+            <div class="__title --active --visible"
                  data-swap-target="#toggle_' . $id . 'a"
                  data-swap-group="accordion_' . $id . '"
                  aria-expanded="true">
               Play me some accordion!
             </div>
-            <div class="__content --visible" id="toggle_' . $id . 'a" data-reveal data-swap-active="true">
+            <div class="__content" id="toggle_' . $id . 'a" data-swap-active="true">
               <div class="__copy">
                 <p>To show an open panel by default, include the class <code>--visible</code> with <code>__content</code>. Omit <code>--visible</code> to load the accordion with all panels closed.</p>
                 <p>Etiam porta sem malesuada magna mollis euismod. Curabitur blandit tempus porttitor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
@@ -36,7 +36,7 @@ function accordion($id, $variant, $section_classes, $acc_class) {
                  aria-expanded="false">
                   Do you want to play some polka?
             </div>
-            <div class="__content" id="toggle_' . $id . 'b" data-reveal>
+            <div class="__content" id="toggle_' . $id . 'b">
               <div class="__copy">
                 <h3>With a Heading Now</h3>
                 <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.</p>
@@ -51,7 +51,7 @@ function accordion($id, $variant, $section_classes, $acc_class) {
                  aria-expanded="false">
                   Roll out the barrel!
             </div>
-            <div class="__content" id="toggle_' . $id . 'c" data-reveal>
+            <div class="__content" id="toggle_' . $id . 'c">
               <div class="__copy">
                 <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.</p>
                 <p>Etiam porta sem malesuada magna mollis euismod. Curabitur blandit tempus porttitor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
--- src/scripts/components/accordion.js
+++ src/scripts/components/accordion.js
@@ -4,11 +4,20 @@ let $acc = $('.accordion [data-swap-target]');
 
 $acc
   .swap()
+  .on('deactivate.swap', function() {
+    $(this).removeClass('--active')
+    $($(this).attr('data-swap-target')).slideUp(600)
+  })
   .on('activate.swap', function() {
     $(this).addClass('--active')
     $($(this).attr('data-swap-target')).slideDown(600)
   })
-  .on('deactivate.swap', function() {
-    $(this).removeClass('--active')
-    $($(this).attr('data-swap-target')).slideUp(600)
+  .on('click touch', function() {
+    let isVisible = $(this).closest('.accordion').find('.--visible')
+    if(isVisible){
+      $($(isVisible).attr('data-swap-target')).slideUp(600)
+      setTimeout(function() {
+        $(isVisible).removeClass('--visible').removeClass('--active')
+      }, 600)
+    }
   });
