773dfe7b78a75e269cf7743686e442e2655592c7
rkingston <r.kingston@northeastern.edu>
2018-07-23 17:11:01 -0400

    update cards with tspt variation per breakpoint

--- docs/views/parts/cards.php
+++ docs/views/parts/cards.php
@@ -77,10 +77,10 @@
     </div>
 
     <!-- mixed ratios and orientations at multiple breakpoints -->
-    <div class="col w--1/3@d w--1/2@w">
-      <article class="card --h --v@d --h@w">
+    <div class="col w--1/3@d">
+      <article class="card --h --v@d">
         <a href="#" class="__link">
-          <div class="__graphic --1x1 --16x9@w"></div>
+          <div class="__graphic --1x1"></div>
           <div class="__body">
             <h2 class="__title">Card With Image and Footer</h2>
             <p>This example has an excerpt of 93 characters. Etiam porta sem malesuada magna mollis euismod.</p>
@@ -91,10 +91,10 @@
         </a>
       </article>
     </div>
-    <div class="col w--1/3@d w--1/2@w">
-      <article class="card --h --right --v@d --h@w">
+    <div class="col w--1/3@d">
+      <article class="card --h --right --v@d">
         <a href="#" class="__link">
-          <div class="__graphic --1x1@t --4x6@w"></div>
+          <div class="__graphic --1x1@t"></div>
           <div class="__body">
             <h2 class="__title">Image On Right</h2>
             <p>This example has an excerpt of only 50 characters.</p>
@@ -105,10 +105,10 @@
         </a>
       </article>
     </div>
-    <div class="col w--1/3@d w--100@w">
-      <article class="card --h --v@d --h@w">
+    <div class="col w--1/3@d">
+      <article class="card --h --v@d">
         <a href="#" class="__link">
-          <div class="__graphic --1x1@d --4x6@w"></div>
+          <div class="__graphic --1x1@d"></div>
           <div class="__body">
             <h2 class="__title">This Card Has an Image and an Exceptionally Lengthy Title</h2>
             <p>This example has an excerpt of 208 characters. Etiam porta sem malesuada magna mollis euismod. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
