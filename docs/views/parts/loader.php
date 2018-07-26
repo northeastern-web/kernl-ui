5aafa4dbcc0da8349bfe43734ae88473c6f2fc08
rkingston <r.kingston@northeastern.edu>
2018-07-26 14:10:57 -0400

    Tweaked loader animation

--- docs/views/parts/loader.php
+++ docs/views/parts/loader.php
@@ -3,7 +3,7 @@
   <p class="ta--c"><a class="btn --lg --pill bc--blue-dark k_loader_example" data-load href="?part=loader">Load Something</a></p>
   <div class="row">
     <div class="col w--1/2@t ow--1/4@t">
-      <div class="card d--flex flex--middle vh--30 bg--gray-100">
+      <div class="card d--flex flex--middle vh--30 bg--gray-100 bs--none">
         <i data-feather="loader" class="load --thin"></i>
         <div class="k_loader-placeholder"></div>
       </div>
