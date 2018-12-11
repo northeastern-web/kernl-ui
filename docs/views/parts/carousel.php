9eb34418e158db7894bf4021e5da3c7fe7860710
rkingston <r.kingston@northeastern.edu>
2018-12-11 12:26:02 -0500

    Add formstone carousel; misc tweaks

---
+++ docs/views/parts/carousel.php
@@ -0,0 +1,32 @@
+<section class="section">
+  <header class="k_section__header">Carousel</header>
+
+      <div class="carousel">
+        <div class="card bg--blue-dark ta--c fs--lead">
+          <div class="__body">
+            1
+          </div>
+        </div>
+        <div class="card bg--blue-dark ta--c fs--lead">
+          <div class="__body">
+            2
+          </div>
+        </div>
+        <div class="card bg--blue-dark ta--c fs--lead">
+          <div class="__body">
+            3
+          </div>
+        </div>
+        <div class="card bg--blue-dark ta--c fs--lead">
+          <div class="__body">
+            4
+          </div>
+        </div>
+        <div class="card bg--blue-dark ta--c fs--lead">
+          <div class="__body">
+            5
+          </div>
+        </div>
+      </div>
+
+</section>
