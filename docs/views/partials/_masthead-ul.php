0bd4dd5459937f94f22f48d4fe781eb92a29d293
rkingston <r.kingston@northeastern.edu>
2019-02-28 13:32:40 -0500

    Fix masthead overlay nav tabs, a11y tweaks

--- docs/views/partials/_masthead-ul.php
+++ docs/views/partials/_masthead-ul.php
@@ -39,23 +39,23 @@ function masthead($id, $masthead_classes, $banner_classes)
       <li class="__item +children --active">
         <a class="__link" href="' . $url .'" aria-haspopup="true" aria-expanded="false">Bibendum</a>
         <ul class="__submenu">
-          <li class="__item"><a class="__link" href="' . $url .'" tabindex="-1">Quam Vulputate Nibh</a></li>
-          <li class="__item"><a class="__link" href="' . $url .'" tabindex="-1">Tortor Fusce</a></li>
+          <li class="__item"><a class="__link" href="' . $url .'">Quam Vulputate Nibh</a></li>
+          <li class="__item"><a class="__link" href="' . $url .'">Tortor Fusce</a></li>
         </ul>
       </li>
       <li class="__item +children">
         <a class="__link" href="' . $url .'" aria-haspopup="true" aria-expanded="false">Pudgi Homunculi</a>
         <ul class="__submenu">
-          <li class="__item"><a class="__link" href="' . $url .'" tabindex="-1">Malesuada Bibendum</a></li>
-          <li class="__item"><a class="__link" href="' . $url .'" tabindex="-1">Bibendum Mattis Dapibus</a></li>
-          <li class="__item"><a class="__link" href="' . $url .'" tabindex="-1">Venenatis Pharetra Sit Dolor</a></li>
+          <li class="__item"><a class="__link" href="' . $url .'">Malesuada Bibendum</a></li>
+          <li class="__item"><a class="__link" href="' . $url .'">Bibendum Mattis Dapibus</a></li>
+          <li class="__item"><a class="__link" href="' . $url .'">Venenatis Pharetra Sit Dolor</a></li>
         </ul>
       </li>
       <li class="__item +children">
         <a class="__link" href="' . $url .'" aria-haspopup="true" aria-expanded="false">Malesuada Nucleus</a>
         <ul class="__submenu">
-          <li class="__item"><a class="__link" href="' . $url .'" tabindex="-1">Quam Vulputate Nibh</a></li>
-          <li class="__item"><a class="__link" href="' . $url .'" tabindex="-1">Tortor Fusce</a></li>
+          <li class="__item"><a class="__link" href="' . $url .'">Quam Vulputate Nibh</a></li>
+          <li class="__item"><a class="__link" href="' . $url .'">Tortor Fusce</a></li>
         </ul>
       </li>
       <li class="__item +icon">
@@ -66,9 +66,11 @@ function masthead($id, $masthead_classes, $banner_classes)
     </ul>
   </nav>
 </header>
-<div class="section bg--img --hero --center ' . $banner_classes . '">
-  <div class="__header">
-    <h2 class="__title py--5@xs">{ <i>Example Hero</i> }</h2>
+<main id="main_content">
+  <div class="section bg--img --hero --center ' . $banner_classes . '">
+    <div class="__header">
+      <h2 class="__title py--5@xs">{ <i>Example Hero</i> }</h2>
+    </div>
   </div>
-</div>';
+</main>';
 }
