fae89e9ea963e494cf886fd4320609a526619098
rkingston <r.kingston@northeastern.edu>
2019-03-06 15:37:43 -0500

    Revert to button; clean up js

--- src/scripts/components/accordion.js
+++ src/scripts/components/accordion.js
@@ -2,48 +2,34 @@
 // Accordion
 
 
-import $ from 'jquery';
+import $ from 'jquery'
 
-let speed = 500;
-let $accordion = $('.accordion [data-swap-target].__title');
+let speed = 500
+let $accordion = $('.accordion [data-swap-target].__title')
 
 // Initialize Formstone swap()
-$accordion.swap();
-
-// Trigger click/touch
-$accordion.on('click touch', function() {
-  // let isVisible = $(this).closest('.accordion').find('.--visible')
-  // if(isVisible){
-  //   $($(isVisible).attr('data-swap-target')).slideUp(speed)
-  //   setTimeout(function() {
-  //     $(isVisible).removeClass('--visible').removeClass('--active')
-  //   }, speed)
-  // }
-});
+$accordion.swap()
 
 // Activate event
 $accordion.on('activate.swap', function() {
-  console.log('activate');
-  let $this = $(this);
-  let id = $(this).attr('data-swap-target');
+  let $this = $(this)
+  let id = $(this).attr('data-swap-target')
 
-  $this.addClass('--active');
-  $(id).slideDown(speed);
+  $this.addClass('--active')
+  $(id).slideDown(speed)
 });
 
 // Deactivate event
 $accordion.on('deactivate.swap', function() {
-  console.log('deactivate', $(this));
-  let $this = $(this);
-  let id = $(this).attr('data-swap-target');
+  let $this = $(this)
+  let id = $(this).attr('data-swap-target')
 
   $this.removeClass('--active')
-  $(id).slideUp(speed);
+  $(id).slideUp(speed)
 });
 
 // Activate event
 $accordion.on('activate.swap', function() {
-  // console.log('activate');
   $(this).addClass('--active')
   $($(this).attr('data-swap-target')).slideDown(speed)
 });
