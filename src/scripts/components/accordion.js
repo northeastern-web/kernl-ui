208a564ab567a214b35948e030964ed08bc2390f
Tim Albert <timothy.m.albert@gmail.com>
2019-03-06 16:12:15 -0500

    Remove .__title from trigger

--- src/scripts/components/accordion.js
+++ src/scripts/components/accordion.js
@@ -4,7 +4,7 @@
 import $ from 'jquery';
 
 let speed = 500;
-let $accordion = $('.accordion [data-swap-target].__title');
+let $accordion = $('.accordion [data-swap-target]');
 
 // Initialize Formstone swap()
 $accordion.swap();
