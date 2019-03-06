92aebd44b6fc6f06905a542002c5a2d6a46c7a0c
rkingston <r.kingston@northeastern.edu>
2019-03-06 14:27:21 -0500

    remove functionality; just console

--- src/scripts/components/accordion.js
+++ src/scripts/components/accordion.js
@@ -11,24 +11,26 @@ let $accordion = $('.accordion [data-swap-target]');
 $accordion.swap();
 
 // Trigger click/touch
-$accordion.on('click touch', function(e) {
-  let isVisible = $(this).closest('.accordion').find('.--visible')
-  if(isVisible){
-    $($(isVisible).attr('data-swap-target')).slideUp(speed)
-    setTimeout(function() {
-      $(isVisible).removeClass('--visible').removeClass('--active')
-    }, speed)
-  }
-});
+// $accordion.on('click touch', function(e) {
+//   let isVisible = $(this).closest('.accordion').find('.--visible')
+//   if(isVisible){
+//     $($(isVisible).attr('data-swap-target')).slideUp(speed)
+//     setTimeout(function() {
+//       $(isVisible).removeClass('--visible').removeClass('--active')
+//     }, speed)
+//   }
+// });
 
 // Deactivate event
 $accordion.on('deactivate.swap', function() {
-  $(this).removeClass('--active')
-  $($(this).attr('data-swap-target')).slideUp(speed)
+  console.log('deactivate');
+  // $(this).removeClass('--active')
+  // $($(this).attr('data-swap-target')).slideUp(speed)
 });
 
 // Activate event
 $accordion.on('activate.swap', function() {
-  $(this).addClass('--active')
-  $($(this).attr('data-swap-target')).slideDown(speed)
+  console.log('activate');
+  // $(this).addClass('--active')
+  // $($(this).attr('data-swap-target')).slideDown(speed)
 });
