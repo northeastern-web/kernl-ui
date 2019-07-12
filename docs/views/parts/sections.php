4b9010acdc944583622173782896e9280dfa1ac0
Tim Albert <timothy.m.albert@gmail.com>
2019-07-12 13:24:52 -0400

    Minor example update

--- docs/views/parts/sections.php
+++ docs/views/parts/sections.php
@@ -4,26 +4,36 @@
   <h2 class="k_section__header">Sections &amp; Headers</h2>
 </header>
 
-<section class="section">
+<section class="section -lg">
   <div class="section_header">
     <h1 class="section_title">Section Header</h1>
-    <div class="section_subtitle">Includes Background Image</div>
-    <p>.flex-middle on section, __body has defined width</p>
+    <div class="section_subtitle">Generic look and feel with columns following</div>
   </div>
+  <div class="row">
+    <div class="col w-1/3@t">
+      Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras justo odio, dapibus ac facilisis in, egestas eget quam
+    </div>
+    <div class="col w-1/3@t">
+      Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras justo odio, dapibus ac facilisis in, egestas eget quam
+    </div>
+    <div class="col w-1/3@t">
+      Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras justo odio, dapibus ac facilisis in, egestas eget quam
+    </div>
+</div>
 </section>
 
-<section class="section -banner -bal vh-100 bg-img">
+<section class="section -banner -bal bg-img">
   <div class="section_header">
-    <div class="section_pretitle"><a href="#">Header Pre Title</a></div>
-    <h1 class="section_title">Header: 100vh, Default (Middle) Position</h1>
-    <div class="section_subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
+    <div class="section_pretitle"><a href="#">Banner Pre Title</a></div>
+    <h1 class="section_title">Banner</h1>
+    <div class="section_subtitle">Banner Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
   </div>
 </section>
 
 <section class="section -lg -bal -center -measure">
   <div class="section_header">
-    <div class="section_pretitle">Header Pre Title</div>
-    <h1 class="section_title">Header with Measure</h1>
+    <div class="section_pretitle">Cras justo odio</div>
+    <h1 class="section_title">Centered with Measure</h1>
     <div class="section_subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
   </div>
 </section>
