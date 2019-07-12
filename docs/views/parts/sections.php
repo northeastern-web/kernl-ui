eb4f52b9dc17a168aa57b1f28acc789c4149eb60
Tim Albert <timothy.m.albert@gmail.com>
2019-07-12 10:05:31 -0400

    Half refactor of section

--- docs/views/parts/sections.php
+++ docs/views/parts/sections.php
@@ -1,45 +1,45 @@
-<!-- ADD Header-FIXED TO THIS PAGE, REQUIRING 16X9 -->
+<!- ADD Header-FIXED TO THIS PAGE, REQUIRING 16X9 ->
 
 <header class="section">
   <h2 class="k_section__header">Sections &amp; Headers</h2>
 </header>
 
-<section class="section --hero bg-img">
-  <div class="__header">
-    <h1 class="__title">Hero</h1>
-    <div class="__subtitle">Includes Background Image</div>
+<section class="section">
+  <div class="section_header">
+    <h1 class="section_title">Section Header</h1>
+    <div class="section_subtitle">Includes Background Image</div>
     <p>.flex-middle on section, __body has defined width</p>
   </div>
 </section>
 
-<section class="section --banner vh-100 bg-img --bal">
-  <div class="__header">
-    <div class="__pretitle"><a href="#">Header Pre Title</a></div>
-    <h1 class="__title">Header: 100vh, Default (Middle) Position</h1>
-    <div class="__subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
+<section class="section -banner -bal vh-100 bg-img">
+  <div class="section_header">
+    <div class="section_pretitle"><a href="#">Header Pre Title</a></div>
+    <h1 class="section_title">Header: 100vh, Default (Middle) Position</h1>
+    <div class="section_subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
   </div>
 </section>
 
-<section class="section --banner --top vh-70 bg-img">
-  <div class="__header">
-    <div class="__pretitle">Header Pre Title</div>
-    <h1 class="__title">Header: 70vh, Top Position</h1>
-    <div class="__subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
+<section class="section -banner -top vh-70 bg-img">
+  <div class="section_header">
+    <div class="section_pretitle">Header Pre Title</div>
+    <h1 class="section_title">Header: 70vh, Top Position</h1>
+    <div class="section_subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
   </div>
 </section>
 
-<section class="section --banner vh-80 --bottom bg-img">
-  <div class="__header">
-    <div class="__pretitle">Header Pre Title</div>
-    <h1 class="__title">Header: Bottom Position, 80vh</h1>
-    <div class="__subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
+<section class="section -banner vh-80 -bottom bg-img">
+  <div class="section_header">
+    <div class="section_pretitle">Header Pre Title</div>
+    <h1 class="section_title">Header: Bottom Position, 80vh</h1>
+    <div class="section_subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
   </div>
 </section>
 
-<section class="section --banner bg-fixed vh-60 bg-img">
-  <div class="__header">
-    <div class="__pretitle">Header Pre Title</div>
-    <h1 class="__title">Header: Large, Fixed, 60vh</h1>
-    <div class="__subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
+<section class="section -banner bg-fixed vh-60 bg-img">
+  <div class="section_header">
+    <div class="section_pretitle">Header Pre Title</div>
+    <h1 class="section_title">Header: Large, Fixed, 60vh</h1>
+    <div class="section_subtitle">Header Sub Title Cras justo odio, dapibus ac facilisis in, egestas eget quam</div>
   </div>
 </section>
