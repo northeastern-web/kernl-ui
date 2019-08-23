11699f6e872cec987dc4289d43864ab8214b1bfd
Tim Albert <timothy.m.albert@gmail.com>
2019-08-23 18:48:50 -0400

    Add tabs

--- docs/views/parts/navs.php
+++ docs/views/parts/navs.php
@@ -122,3 +122,45 @@
 
   </div>
 </section>
+
+<section class="section">
+  <div class="row">
+    <div class="col" role="tablist">
+      <nav class="nav -inline" aria-label="Tabs">
+        <ul class="nav_list">
+          <li class="nav_item">
+            <a id="tab_0_1" href="#tab_1" class="nav_link" aria-selected="true" data-toggle="tab" role="tab" data-swap-group="tabgroup_1">Dolor Fermentum</a>
+          </li>
+          <li class="nav_item">
+            <a id="tab_0_2" href="#tab_2" class="nav_link" data-toggle="tab" role="tab" data-swap-group="tabgroup_1">Mattis Euismod</a>
+          </li>
+          <li class="nav_item">
+            <a id="tab_0_3" href="#tab_3" class="nav_link" data-toggle="tab" role="tab" data-swap-group="tabgroup_1">Mollis Purus</a>
+          </li>
+          <li class="nav_item">
+            <a id="tab_0_4" href="#tab_4" class="nav_link" data-toggle="tab" role="tab" data-swap-group="tabgroup_1">Sit Parturient</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="pt-1">
+        <div id="tab_1" role="tabpanel" aria-hidden="false" aria-labelledby="tab_0_1">
+          <h3>Content 1</h3>
+          <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div id="tab_2" role="tabpanel" aria-hidden="true" aria-labelledby="tab_0_2">
+          <h3>Content 2</h3>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur.</p>
+        </div>
+        <div id="tab_3" role="tabpanel" aria-hidden="true" aria-labelledby="tab_0_3">
+          <h3>Content 3</h3>
+          <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+        </div>
+        <div id="tab_4" role="tabpanel" aria-hidden="true" aria-labelledby="tab_0_4">
+          <h3>Content 4</h3>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor. Donec id elit non mi porta gravida at eget metus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section>
