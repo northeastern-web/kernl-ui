b0936e03c45fc2aa91dc39061815b2cf857de9be
Tim Albert <timothy.m.albert@gmail.com>
2019-08-28 11:23:50 -0400

    Add second modal example

--- docs/views/parts/modals.php
+++ docs/views/parts/modals.php
@@ -3,20 +3,31 @@
 
   <div class="row">
     <div class="col w-1/3@t">
-      <p class="modal-demo ta-c">
-        <a class="btn" href="#modal_1" data-size="sm" data-toggle>Modal</a>
-      </p>
-
-      <div class="modal" id="modal_1" tabindex="-1" role="dialog" aria-labelledby="modal_base_label" aria-hidden="true">
+      <a class="btn" id="modal_trigger_1" href="#modal_1" data-toggle>Modal 1</a>
+      <div class="modal" id="modal_1" tabindex="-1" role="dialog" aria-labelledby="modal_trigger_1" aria-hidden="true">
         <div class="modal_screen" data-dismiss="modal"></div>
         <div class="modal_body bg-white">
+          <h2>Modal 1</h2>
           <p>Nulla vitae elit libero, a pharetra augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur blandit tempus porttitor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vestibulum id ligula porta felis euismod semper. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+        </div>
+        <button type="button" class="modal_close" toggle-dismiss aria-label="Close">
+          <i data-feather="x"></i>
+        </button>
+      </div>
 
-          <p>Vestibulum id ligula porta felis euismod semper. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nulla vitae elit libero, a pharetra augue.</p>
-
-          <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies vehicula ut id elit. Nulla vitae elit libero, a pharetra augue.</p>
-
-          <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec id elit non mi porta gravida at eget metus. Donec id elit non mi porta gravida at eget metus.</p>
+      <a class="btn" id="modal_trigger_2" href="#modal_2" data-toggle>Modal 2</a>
+      <div class="modal" id="modal_2" tabindex="-1" role="dialog" aria-labelledby="modal_trigger_2" aria-hidden="true">
+        <div class="modal_screen" data-dismiss="modal"></div>
+        <div class="modal_body bg-beige">
+          <h2 class="ta-c">Modal 2</h2>
+          <div class="row">
+            <div class="col w-1/2">
+              <p>Nulla vitae elit libero, a pharetra augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur blandit tempus porttitor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vestibulum id ligula porta felis euismod semper. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+            </div>
+            <div class="col w-1/2">
+              <p>Nulla vitae elit libero, a pharetra augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur blandit tempus porttitor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vestibulum id ligula porta felis euismod semper. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+            </div>
+          </div>
         </div>
         <button type="button" class="modal_close" toggle-dismiss aria-label="Close">
           <i data-feather="x"></i>
