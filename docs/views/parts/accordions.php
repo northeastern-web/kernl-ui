fec074db1758018c00d31e847b3a8d3595efd37a
Tim Albert <timothy.m.albert@gmail.com>
2019-09-06 12:37:41 -0400

    Various standardizations

--- docs/views/parts/accordions.php
+++ docs/views/parts/accordions.php
@@ -19,10 +19,17 @@ $bg_param = (isset($_GET['bg']) ? $_GET['bg'] : null);
         </div>
       </div>
       <div class="accordion">
-        <a id="toggle_0_2" href="#" data-swap-target="#accordion_0_2" class="accordion_link" data-toggle data-swap-options='{"collapse":true}' role="tab" data-swap-group="accordiongroup_0">Dolor Fermentum</a>
+        <a id="toggle_0_2" href="#" data-swap-target="#accordion_0_2" class="accordion_link" data-toggle data-swap-options='{"collapse":true}' role="tab" data-swap-group="accordiongroup_0">Sed posuere</a>
         <div id="accordion_0_2" class="accordion_body" role="tabpanel" aria-hidden="true" aria-labelledby="toggle_0_2">
           <h3>Content 2</h3>
           <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+      <div class="accordion">
+        <a id="toggle_0_3" href="#" data-swap-target="#accordion_0_3" class="accordion_link" data-toggle data-swap-options='{"collapse":true}' role="tab" data-swap-group="accordiongroup_0">Integer posuere</a>
+        <div id="accordion_0_3" class="accordion_body" role="tabpanel" aria-hidden="true" aria-labelledby="toggle_0_3">
+          <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur blandit tempus porttitor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+          <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur blandit tempus porttitor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
         </div>
       </div>
     </div>
