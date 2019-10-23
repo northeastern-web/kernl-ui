06b8ceeda377fe0209837b42b9efe3e294298edd
Tim Albert <timothy.m.albert@gmail.com>
2019-10-23 16:33:23 -0400

    Escape ?part= query string

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
-<?php $part = (isset($_GET['part']) ? $_GET['part'] : null); ?>
-<?php include 'functions.php'; ?>
+<?php
+$part = filter_var($_GET['part'], FILTER_SANITIZE_STRING);
+$part = (isset($part) ? $part : null);
+include 'functions.php';
+?>
 
 <!doctype html>
 <html id="kernl">
-  <?php include 'docs/views/_head.php'; ?>
-  <body class="chrm<?= $part ? ' part--' . $part : ' home'; ?>">
+<?php include 'docs/views/_head.php'; ?>
+
+<body class="chrm<?= $part ? ' part--' . $part : ' home'; ?>">
     <?php include 'docs/views/_header.php'; ?>
 
     <div class="k_wrap" id="main_content" role="document">
-      <main class="k_main main" role="document">
-        <?php include 'docs/views/_index.php'; ?>
-      </main>
-      <?php include 'docs/views/_nav.php'; ?>
+        <main class="k_main main" role="document">
+            <?php include 'docs/views/_index.php'; ?>
+        </main>
+        <?php include 'docs/views/_nav.php'; ?>
     </div>
 
     <?php include 'docs/views/_footer.php'; ?>
-  </body>
+</body>
+
 </html>
