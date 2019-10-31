a44adca637814610ac730c02295b3f4b9a203aa8
Tim Albert <timothy.m.albert@gmail.com>
2019-10-31 14:20:35 -0400

    Add footer JS to docs

--- docs/views/_footer.php
+++ docs/views/_footer.php
@@ -3,4 +3,5 @@
   <p><a class="k_logo" href="/"><b>kernl</b>(ui)</a></p>
 </footer>
 
+<script type="text/javascript" src="https://www.northeastern.edu/nuglobalutils/common/js/navigation.js"></script>
 <script src="<?= $url; ?>/docs/dist/scripts/docs.js"></script>
