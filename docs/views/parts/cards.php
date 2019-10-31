df6bb6a0b5ab3681c9231286934ed1fb745fdb8d
Tim Albert <timothy.m.albert@gmail.com>
2019-10-31 16:00:48 -0400

    Add cascade to _body and _subtitle for -profile

--- docs/views/parts/cards.php
+++ docs/views/parts/cards.php
@@ -48,7 +48,7 @@
     <div class="col w-1/2@t w-1/3@d">
       <article class="card">
         <a class="card_link" href="#">
-        <div class="card_media"></div>
+          <div class="card_media"></div>
           <div class="card_body">
             <h2 class="card_title">Image Card Title</h2>
             <p>Card content lorem ipsum</p>
@@ -104,7 +104,7 @@
     <div class="col w-1/2@t w-1/4@d">
       <article class="card -overlay -middle">
         <a class="card_link" href="#">
-        <div class="card_media"></div>
+          <div class="card_media"></div>
           <div class="card_body">
             <h2 class="card_title">Overlay Card Title</h2>
             <p>Card content lorem ipsum</p>
@@ -127,7 +127,7 @@
     <div class="col w-1/2@t w-1/4@d">
       <article class="card -overlay">
         <a class="card_link" href="#">
-        <div class="card_media"></div>
+          <div class="card_media"></div>
           <div class="card_body">
             <h2 class="card_title">Overlay Card Title</h2>
             <p>Card content lorem ipsum</p>
@@ -157,7 +157,7 @@
     </div>
   </div>
 
-    <!-- orientation demo -->
+  <!-- orientation demo -->
   <div class="row">
     <div class="col w-1/2@t">
       <article class="card -h -v@d -h@w">
@@ -190,16 +190,16 @@
   </div>
 
 
-    <!-- Profile -->
+  <!-- Profile -->
 
   <div class="row">
     <div class="col w-1/2 w-1/3@t w-20@d">
       <article class="card -profile group">
         <a href="#" class="card_link">
           <div class="card_media ar-1x1"></div>
-          <div class="card_body py-1">
+          <div class="card_body">
             <h2 class="card_title">Kay Rooney</h2>
-            <div class="card_subtitle fs-italic pb-0h">School of HummaJamma BoobaDooda, University of Edinburgh</div>
+            <div class="card_subtitle">School of HummaJamma BoobaDooda, University of Edinburgh</div>
             <p>General Educational Development (GED) Examiner</p>
           </div>
           <div class="card_footer">
@@ -212,9 +212,9 @@
       <article class="card -profile group">
         <a href="#" class="card_link">
           <div class="card_media ar-1x1"></div>
-          <div class="card_body py-1">
+          <div class="card_body">
             <h2 class="card_title">Cooper O'Neill</h2>
-            <div class="card_subtitle fs-italic pb-0h">School of HummaJamma BoobaDooda, Boston College</div>
+            <div class="card_subtitle">School of HummaJamma BoobaDooda, Boston College</div>
             <p>Individualized Education Plan (IEP) Aide</p>
           </div>
           <div class="card_footer">
@@ -227,14 +227,14 @@
       <article class="card -profile group">
         <a href="#" class="card_link">
           <div class="card_media ar-1x1"></div>
-          <div class="card_body py-1">
+          <div class="card_body">
             <h2 class="card_title">Lillie Squires (Lil)</h2>
-            <div class="card_subtitle fs-italic pb-0h">School of HummaJamma BoobaDooda, MIT</div>
+            <div class="card_subtitle">School of HummaJamma BoobaDooda, MIT</div>
             <p>Assistant Professor, Communication Studies and Faculty Affiliate, Women's', Gender, &amp; Sexuality Studies Program and Department of Cultures, Societies, &amp; Global Studies</p>
           </div>
           <div class="card_footer">
             <div class="card_column bwa-0 bwb-2 bc-none group-hover:bc-red"><i data-feather="more-vertical" class="-sm"></i>View Profile</div>
-           </div>
+          </div>
         </a>
       </article>
     </div>
@@ -242,9 +242,9 @@
       <article class="card -profile group">
         <a href="#" class="card_link">
           <div class="card_media ar-1x1"></div>
-          <div class="card_body py-1">
+          <div class="card_body">
             <h2 class="card_title">Millie Harper</h2>
-            <div class="card_subtitle fs-italic pb-0h">School of HummaJamma BoobaDooda, Northeastern University</div>
+            <div class="card_subtitle">School of HummaJamma BoobaDooda, Northeastern University</div>
             <p>Scholastic Aptitude Test (SAT) Grader</p>
           </div>
           <div class="card_footer">
@@ -257,9 +257,9 @@
       <article class="card -profile group">
         <a href="#" class="card_link">
           <div class="card_media ar-1x1"></div>
-          <div class="card_body py-1">
+          <div class="card_body">
             <h2 class="card_title">Elizabeth Stuart (Liza)</h2>
-            <div class="card_subtitle fs-italic pb-0h">College of Computer and Information Science, Northeastern University</div>
+            <div class="card_subtitle">College of Computer and Information Science, Northeastern University</div>
             <p>Associate Dean of Academic Affairs, Diversity and Inclusion and Dean's Professor of Civic Sustainability, College of Social Sciences and Humanities</p>
           </div>
           <div class="card_footer">
