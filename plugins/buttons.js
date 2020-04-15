6f9f802e479548de578361f3c7baf05a958cfd7e
Jason Beggs <jason@roasted.dev>
2020-04-15 12:49:26 -0400

    Add btn-lg

--- plugins/buttons.js
+++ plugins/buttons.js
@@ -1,42 +1,45 @@
 module.exports = function({ addComponents, theme }) {
-    const commonStyles = {
-        display: 'inline-flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        whiteSpace: 'nowrap',
-        borderWidth: '1px',
-        borderColor: 'transparent',
-        transitionProperty: theme('transitions.colors'),
-        transitionDuration: theme('transitionDuration.100'),
-        '&:focus': {
-            outline: 'none',
-            boxShadow: theme('boxShadow.outline'),
-        },
-        '&:disabled': {
-            cursor: 'not-allowed',
-            opacity: '0.75',
-            pointerEvents: 'none',
-        },
-        '&[disabled]': {
-            cursor: 'not-allowed',
-            opacity: '0.75',
-            pointerEvents: 'none',
-        },
-    };
+  const commonStyles = {
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: theme('spacing.3') + ' ' + theme('spacing.6'),
+    textAlign: 'center',
+    whiteSpace: 'nowrap',
+    lineHight: '1rem',
+    borderWidth: '1px',
+    borderColor: 'transparent',
+    transitionProperty: theme('transitions.colors'),
+    transitionDuration: theme('transitionDuration.100'),
+    '&:focus': {
+      outline: 'none',
+      boxShadow: theme('boxShadow.outline'),
+    },
+    '&:disabled': {
+      cursor: 'not-allowed',
+      opacity: '0.75',
+      pointerEvents: 'none',
+    },
+    '&[disabled]': {
+      cursor: 'not-allowed',
+      opacity: '0.75',
+      pointerEvents: 'none',
+    },
+  }
 
-    const components = {
-        '.btn': {
-            padding: theme('spacing.3') + ' ' + theme('spacing.6'),
-            fontSize: theme('fontSize.sm'),
-            ...commonStyles,
-        },
-        '.btn-sm': {
-            padding: theme('spacing.2') + ' ' + theme('spacing.3'),
-            fontSize: theme('fontSize.xs'),
-            ...commonStyles,
-        },
-    };
+  const components = {
+    '.btn': {
+      fontSize: theme('fontSize.sm'),
+      ...commonStyles,
+    },
+    '.btn-sm': {
+      fontSize: theme('fontSize.xs'),
+      ...commonStyles,
+    },
+    '.btn-lg': {
+      ...commonStyles,
+    },
+  }
 
-    addComponents(components);
-};
+  addComponents(components)
+}
