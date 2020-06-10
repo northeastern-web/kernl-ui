20616d0ee77583e263263a586fbfcf275e431354
Jason Beggs <jason@roasted.dev>
2020-06-10 15:12:52 -0400

    Use 16px rem on all screens

--- plugins/base.js
+++ plugins/base.js
@@ -4,19 +4,6 @@ const defaultConfig = require('../defaultConfig')
 module.exports = plugin(function({ addBase, addComponents, theme }) {
   const baseStyles = {
     "@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900');": {},
-    'html, body': {
-      fontSize: '15px',
-    },
-    '@screen md': {
-      'html, body': {
-        fontSize: '16px',
-      },
-    },
-    '@screen lg': {
-      'html, body': {
-        fontSize: '17px',
-      },
-    },
     '[x-cloak]': {
       display: 'none',
     },
