f923cfa3bb51209727e2586d93822030adfd945b
Jason Beggs <jason@roasted.dev>
2020-06-26 08:43:26 -0400

    Rename CSS file

--- scripts/build.js
+++ scripts/build.js
@@ -5,25 +5,25 @@ const CleanCSS = require('clean-css')
 
 console.log('Building kernl(ui) CSS...')
 
-fs.readFile(`./src/css/main.css`, (err, css) => {
+fs.readFile(`./src/css/index.css`, (err, css) => {
   if (err) throw err
 
   return postcss([tailwind('./cdn.config.js')])
     .process(css, {
-      from: `./src/css/main.css`,
-      to: `./dist/css/main.css`,
+      from: `./src/css/index.css`,
+      to: `./dist/css/index.css`,
       map: { inline: false },
     })
     .then((result) => {
-      fs.writeFileSync(`./dist/css/main.css`, result.css)
+      fs.writeFileSync(`./dist/css/index.css`, result.css)
       if (result.map) {
-        fs.writeFileSync(`./dist/css/main.css.map`, result.map)
+        fs.writeFileSync(`./dist/css/index.css.map`, result.map)
       }
       return result
     })
     .then((result) => {
       const minified = new CleanCSS().minify(result.css)
-      fs.writeFileSync(`./dist/css/main.css`, minified.styles)
+      fs.writeFileSync(`./dist/css/index.css`, minified.styles)
 
       console.log('kernl(ui) CSS built successfully!')
     })
