d6cc88c4fb2c76aeed320d13c246e55dcc2ea4de
Jason Beggs <jason@roasted.dev>
2020-06-26 11:07:42 -0400

    Fix builds

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,7 @@ import 'regenerator-runtime/runtime'
 import Alpine from 'alpinejs/dist/alpine-ie11'
 window.Alpine = Alpine
 
-import 'feather-icons'
+import Feather from 'feather-icons'
+window.Feather = Feather
+
 Feather.replace()
