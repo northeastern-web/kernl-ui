05b0f6f55f01bea9d5ab656a01d2f90946ecef3a
Jason Beggs <jason@roasted.dev>
2020-10-12 09:43:22 -0400

    Remove the loader class

--- plugins/base.js
+++ plugins/base.js
@@ -3,7 +3,7 @@ const defaultConfig = require('../defaultConfig')
 
 module.exports = plugin(function ({ addBase, addComponents, theme }) {
   const baseStyles = {
-    '[x-cloak]': {
+    '[x-cloak], [v-cloak]': {
       display: 'none !important',
     },
     'body .buorg': {
@@ -59,24 +59,6 @@ module.exports = plugin(function ({ addBase, addComponents, theme }) {
         'padding-left': '4rem',
         'padding-right': '4rem',
       },
-    },
-  })
-
-  addComponents({
-    '.loading': {
-      position: 'absolute',
-      display: 'block',
-      animation: 'spin 750ms infinite linear',
-      borderRadius: theme('borderRadius.full'),
-      borderWidth: '0.125em',
-      borderTopColor: 'currentColor',
-      borderRightColor: 'currentColor',
-      borderLeftColor: 'transparent',
-      borderBottomColor: 'transparent',
-      height: '1em',
-      width: '1em',
-      left: 'calc(50% - (1em / 2))',
-      top: 'calc(50% - (1em / 2))',
     },
   })
 }, defaultConfig)
